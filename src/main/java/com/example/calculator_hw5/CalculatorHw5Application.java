f08b9250ac05fb30a2927b16164ab6dbf569cb05
sergeydomanov <nomad-labs@yandex.ru>
2022-11-21 20:09:56 +0300

    first commit

---
+++ src/main/java/com/example/calculator_hw5/CalculatorHw5Application.java
@@ -0,0 +1,13 @@
+package com.example.calculator_hw5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorHw5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorHw5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calculator_hw5/CalculatorHw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculator_hw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorHw5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
